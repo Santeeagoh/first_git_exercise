338172c039b3149c618258fe2a696ae22c9ec423
Santeeagoh <santiagom200211@outlook.es>
2023-09-29 20:35:59 -0500

    primera version

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+    // Comentarios**
+$nPerson = 0;
+$valueCover = 20000;
+$consumo = 150000;
+$totalPay = 0;
+
+    // Procedimiento
+
+echo "Digite el numero de personas a ingresar: /n";
+
+$nPerson = readline();
+$totalPay = $nPerson * $valueCover + $consumo;
+
+echo "Su total a pagar es: /n";
+
+echo $totalPay
+
+
+
+
+

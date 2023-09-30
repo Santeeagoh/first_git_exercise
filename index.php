f81710a7a533647b161cbe10336f619a767e7d5c
Santeeagoh <santiagom200211@outlook.es>
2023-09-29 20:58:21 -0500

    Se agrega app.php

--- index.php
+++ index.php
@@ -6,16 +6,25 @@ $valueCover = 20000;
 $consumo = 150000;
 $totalPay = 0;
 
-    // Procedimiento
+    // Procedimiento** Suma de personas por entrada mas consumo dentro del local
 
 echo "Digite el numero de personas a ingresar: /n";
 
 $nPerson = readline();
+$valueNoconsumo = $nPerson * $valueCover;
 $totalPay = $nPerson * $valueCover + $consumo;
+
+    //Salida** valor total 
 
 echo "Su total a pagar es: /n";
 
-echo $totalPay
+echo $valueNoconsumo;
+
+echo "El total con el consumo general es: /n";
+
+echo $totalPay;
+
+?>
 
 
 

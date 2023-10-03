00922cf725b8e15709f3c874c76d76f42fc9f6ae
Santeeagoh <santiagom200211@outlook.es>
2023-10-02 19:28:19 -0500

    Se modifica index.php

--- index.php
+++ index.php
@@ -1,31 +1,6 @@
 <?php
 
-    // Comentarios**
-$nPerson = 0;
-$valueCover = 20000;
-$consumo = 150000;
-$totalPay = 0;
-
-    // Procedimiento** Suma de personas por entrada mas consumo dentro del local
-
-echo "Digite el numero de personas a ingresar: /n";
-
-$nPerson = readline();
-$valueNoconsumo = $nPerson * $valueCover;
-$totalPay = $nPerson * $valueCover + $consumo;
-
-    //Salida** valor total 
-
-echo "Su total a pagar es: /n";
-
-echo $valueNoconsumo;
-
-echo "El total con el consumo general es: /n";
-
-echo $totalPay;
-
 ?>
-
 
 
 
